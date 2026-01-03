db929821527885e44165388eb1a8a3ca6cb77ada
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-01-03 17:13:16 +0530

    chore: update comments in file

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,12 +1,14 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const User = pgTable("usersTable", {
+    // uuid is used as a random unique value
   id: uuid("id").defaultRandom().primaryKey(),
+    // Stores the unique user identifier provided by Google OAuth.
   googleId: text("google_id").notNull().unique(),
   fullName: text("name"),
   email: text("email").notNull().unique(),
   avatarUrl: text("avatar_url"),
-  // timestamps
+
   createdAt: timestamp("created_at", { withTimezone: true })
     .notNull()
     .defaultNow(),
