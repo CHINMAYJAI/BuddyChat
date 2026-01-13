0c08f1c586c90586632d2a97362030cd1ad2e15d
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-01-13 20:24:59 +0530

    refactor(server): move port handling to server and add graceful shutdown of db

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,13 +4,6 @@ import cors from "cors";
 
 const app: Application = express();
 
-// PORT handling
-const PORT: number = Number(process.env.PORT || "8080");
-
-if (isNaN(PORT) || PORT <= 0) {
-  throw new Error("Invalid PORT number in environment variables");
-}
-
 // cors handling and whitelisting
 const FRONTEND_URL = process.env.CORS_ORIGIN;
 
@@ -39,7 +32,7 @@ app.use(express.json());
 
 // health check
 app.get("/health", (req: Request, res: Response) => {
-  res.json({ message: `Server is running at PORT: ${PORT}` });
+  res.json({ status: 'ok' });
 });
 
-export { app, PORT };
+export { app };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,41 @@
-import { app, PORT } from "./app.js";
+import { app } from "./app.js";
 import dotenv from "dotenv";
+import {
+  shutMongoConnection,
+  shutPostgresConnection,
+  shutRedisConnection,
+} from "./lib/db/index.db.js";
+import type { Server } from "http";
 
 dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at PORT: ${PORT}`);
+// PORT handling
+const PORT: number = Number(process.env.PORT || "8080");
+
+if (isNaN(PORT) || PORT <= 0) {
+  throw new Error("Invalid PORT number in environment variables");
+}
+
+const server: Server = app.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
 });
+
+// graceful shutdown
+const shutdown = async (): Promise<void> => {
+  console.log("Shutting down...");
+
+  server.close(async () => {
+    await Promise.allSettled([
+      shutMongoConnection(),
+      shutPostgresConnection(),
+      shutRedisConnection(),
+    ]);
+    console.log("All Server Closed!");
+    setTimeout(() => process.exit(0), 100); // time to flush logs and finish cleanups
+  });
+
+};
+
+// signals
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
