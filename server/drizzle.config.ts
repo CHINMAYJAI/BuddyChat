0582113efd649cc55b743133fc1544753d2570be
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-01-23 03:19:04 +0530

    refactor(config): standardize env loading and validation for server and database

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,13 @@
+import "dotenv/config";
 import "dotenv";
 import { defineConfig } from "drizzle-kit";
+import { config } from "./src/utils/validateEnvVariables.utils";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/models/user.model.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.PG_DATABASE_URL!,
+    url: config.postgres.URL,
   },
 });
--- server/src/utils/validateEnvVariables.utils.ts
+++ server/src/utils/validateEnvVariables.utils.ts
@@ -7,9 +7,12 @@ const required = (key: string): string => {
 };
 
 export const config = {
+  NODE_ENV: required("NODE_ENV"),
+
   app: {
     CORS_ORIGIN: required("CORS_ORIGIN"),
     PORT: Number(required("PORT")),
+    FRONTEND_DASHBOARD_URL: required("FRONTEND_DASHBOARD_URL"),
   },
 
   mongo: {
@@ -19,14 +22,30 @@ export const config = {
 
   postgres: {
     USER: required("PG_USER"),
-    PASSWORD: required("PG_PASSWORD"),
     DATABASE: required("PG_DATABASE"),
+    PASSWORD: required("PG_PASSWORD"),
+    URL: required("PG_DATABASE_URL"),
   },
 
   redis: {
     URL: required("REDIS_URL"),
     USERNAME: required("REDIS_USERNAME"),
     PASSWORD: required("REDIS_PASSWORD"),
+  },
+
+  googleOAuth: {
+    CLIENT_ID: required("GOOGLE_CLIENT_ID"),
+    CLIENT_SECRET: required("GOOGLE_CLIENT_SECRET"),
+    REDIRECT_URI: required("GOOGLE_REDIRECT_URI"),
+  },
+
+  jwt: {
+    SECRET: required("JWT_SECRET"),
+    EXPIRES_IN: required("JWT_EXPIRES_IN"),
+  },
+
+  cookies: {
+    EXPIRES_IN: required("COOKIES_EXPIRES_IN"),
   },
 };
 
