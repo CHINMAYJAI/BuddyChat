34d9334d04deecc7adeba9ef5b831809c46e2f61
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-03-16 12:56:55 +0530

    refactor: moved and rename the file; updated the import statement - moved from utils/ to utils/postgres/ - rename it from user.store.ts to user.postgres.store.ts - updated the import statement file path in googleAuth.auth.service.ts

--- server/src/services/auth/googleAuth.auth.service.ts
+++ server/src/services/auth/googleAuth.auth.service.ts
@@ -1,9 +1,9 @@
 import { verifyGoogleUser } from "./index.auth.service.js";
-import { findOrCreateGoogleUser } from "../../store/user.store.js";
+import { findOrCreateGoogleUser } from "../../store/postgres/user.postgres.store.js";
 import { generateJwt } from "../../utils/jwt.utils.js";
 
 export const handleGoogleCallback = async (code: string): Promise<string> => {
-  const googleUser = await verifyGoogleUser(code);
-  const userId = await findOrCreateGoogleUser(googleUser);
-  return generateJwt(userId);
+    const googleUser = await verifyGoogleUser(code);
+    const userId = await findOrCreateGoogleUser(googleUser);
+    return generateJwt(userId);
 };
