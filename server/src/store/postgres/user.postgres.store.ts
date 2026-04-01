a8e91e707c26c65a86e9f815e288c57e4cd77e14
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-04-01 09:25:05 +0530

    fix: updated import paths

--- server/src/store/postgres/user.postgres.store.ts
+++ server/src/store/postgres/user.postgres.store.ts
@@ -1,32 +1,32 @@
-import { postgresConnectionDB } from "../lib/db/index.db.js";
-import { User } from "../models/index.model.js";
+import { postgresConnectionDB } from "../../lib/db/index.db.js";
+import { User } from "../../models/index.model.js";
 import { eq } from "drizzle-orm";
-import type { GoogleUser } from "../services/auth/googleOAuth.auth.service.js";
+import type { GoogleUser } from "../../services/auth/googleOAuth.auth.service.js";
 
 // If user logs in with google, find them in your database or create them if they don’t exist and return their user ID.
 export const findOrCreateGoogleUser = async (
-  googleUser: GoogleUser,
+    googleUser: GoogleUser
 ): Promise<string> => {
-  const db = postgresConnectionDB();
+    const db = postgresConnectionDB();
 
-  const existing = await db
-    .select()
-    .from(User)
-    .where(eq(User.googleId, googleUser.googleId))
-    .limit(1);
+    const existing = await db
+        .select()
+        .from(User)
+        .where(eq(User.googleId, googleUser.googleId))
+        .limit(1);
 
-  if (existing.length) return existing[0]!.id;
+    if (existing.length) return existing[0]!.id;
 
-  const created = await db
-    .insert(User)
-    .values({
-      googleId: googleUser.googleId,
-      email: googleUser.email,
-      fullName: googleUser.name,
-      avatarUrl: googleUser.avatar,
-    })
-    .returning({ id: User.id });
+    const created = await db
+        .insert(User)
+        .values({
+            googleId: googleUser.googleId,
+            email: googleUser.email,
+            fullName: googleUser.name,
+            avatarUrl: googleUser.avatar,
+        })
+        .returning({ id: User.id });
 
-  if (!created[0]) throw new Error("User creation failed");
-  return created[0].id;
+    if (!created[0]) throw new Error("User creation failed");
+    return created[0].id;
 };
