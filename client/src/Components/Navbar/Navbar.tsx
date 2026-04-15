990622ad3e5eb8f4882a3e1dcf2a91dafeeeb3c8
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-04-15 23:51:48 +0530

    feat(client:Navbar) and fix(client:Navbar): feat - on clicking outside dropdown; dropdown will close fix - on clicking profile icon (desktop)/close icon (mobile) rendering   dropdown will close

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ import {
     forwardArrowIcon,
     backwardArrowIcon,
 } from "@/assets/icons/navbar/index.assets.icons.navbar";
-import { JSX, useState } from "react";
+import { JSX, useState, useEffect, useRef } from "react";
 
 const Navbar: React.FC = () => {
     // useState hook
@@ -22,6 +22,41 @@ const Navbar: React.FC = () => {
     const [desktopMenuOpen, setDesktopMenuOpen] = useState<boolean>(false);
     const [desktopSubOpen, setDesktopSubMenuOpen] = useState<boolean>(false);
     const [searchOpen, setSearchOpen] = useState<boolean>(false);
+
+    // dropdown menu should be close whenever user clicks outside it for better user experience
+
+    // useRef hook
+    const mobileMenuRef = useRef<HTMLDivElement | null>(null);
+    const desktopMenuRef = useRef<HTMLDivElement | null>(null);
+
+    // useEffect
+    useEffect(() => {
+        const closeDropDownMenu = (event: MouseEvent) => {
+            const target = event.target as Node;
+            // menubar
+            // if renders on mobile
+            if (
+                mobileMenuRef.current &&
+                !mobileMenuRef.current.contains(target)
+            ) {
+                setMobileSubMenuOpen(false);
+                setMobileMenuOpen(false);
+            }
+            // if renders on desktop
+            if (
+                desktopMenuRef.current &&
+                !desktopMenuRef.current.contains(target)
+            ) {
+                setDesktopSubMenuOpen(false);
+                setDesktopMenuOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", closeDropDownMenu);
+        // cleanup
+        return () => {
+            document.removeEventListener("mousedown", closeDropDownMenu);
+        };
+    }, []);
 
     // handlers
     const openDesktopSubMenu = (): void => setDesktopSubMenuOpen(true);
@@ -107,7 +142,7 @@ const Navbar: React.FC = () => {
 
     // sub menu
     const desktopSubMenu: MenuRenderer = () => {
-        if (!desktopSubOpen) return null;
+        if (!desktopMenuOpen || !desktopSubOpen) return null;
 
         return (
             <div className="dropdown-menu dropdown-right">
@@ -132,7 +167,7 @@ const Navbar: React.FC = () => {
     };
 
     const mobileSubMenu: MenuRenderer = () => {
-        if (!mobileSubOpen) return null;
+        if (!mobileMenuOpen || !mobileSubOpen) return null;
 
         return (
             <div className="dropdown-menu">
@@ -159,7 +194,7 @@ const Navbar: React.FC = () => {
     return (
         <div className="navbar">
             {/* LEFT */}
-            <div className="nav-left">
+            <div className="nav-left" ref={mobileMenuRef}>
                 <button
                     className="icon-btn hamburger-btn"
                     onClick={() => setMobileMenuOpen((prev) => !prev)}
@@ -202,7 +237,7 @@ const Navbar: React.FC = () => {
             </div>
 
             {/* RIGHT */}
-            <div className="nav-right desktop-only">
+            <div className="nav-right desktop-only" ref={desktopMenuRef}>
                 <div
                     className="profile-circle"
                     onClick={() => setDesktopMenuOpen((prev) => !prev)}
