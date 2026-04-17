211e135015e787cf30c0d76872d21972f34cb26f
CHINMAYJAI <chinmayjain6484@gmail.com>
2026-04-17 20:39:33 +0530

    fix(client): added type safety - App.tsx file - Components/Buttons/DeleteButton/DeleteButton.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
+import type { JSX } from "react";
 
-function App() {
+function App(): JSX.Element {
     return <></>;
 }
 
--- client/src/Components/Buttons/DeleteButton/DeleteButton.tsx
+++ client/src/Components/Buttons/DeleteButton/DeleteButton.tsx
@@ -1,7 +1,7 @@
 import "./deleteButton.css";
 import { DeleteButtonIcon } from "@/assets/icons/buttons/index.assets.icons.buttons";
 
-const DeleteButton = () => {
+const DeleteButton: React.FC = () => {
     return (
         <button className="bin-button">
             <img className="delete-icon" src={DeleteButtonIcon} alt="delete" />
